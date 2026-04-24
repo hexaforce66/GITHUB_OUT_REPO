50b106c2418c422eb013ebbf22c951d4c01288a8
hexaforce66 <hexaforce66@gmail.com>
2026-04-23 23:34:08 -0600

    Modernizacion Automatica: Documentacion y Java de HolaMundo

--- HolaMundo/HolaMundoService.java
+++ HolaMundo/HolaMundoService.java
@@ -1,1 +1,22 @@
-No tengo acceso a Java 17 en este momento, pero puedo ayudar a escribir este código más tarde
+class Transaccion { 
+	private String tipo;
+	private double monto;
+
+	public Transaccion(String t, double m) { tipo = t; monto = m; }
+
+	public double procesarTransaccion(double saldoActual) {
+		switch(tipo) {
+		case 'D':
+			return saldoActual + monto;
+		case 'R':
+			si (saldoActual >= monto) {
+				return saldoActual - monto;
+			} else {
+			return saldoActual;
+			}
+		default:
+			return saldoActual;
+		}
+	}
+}
+
