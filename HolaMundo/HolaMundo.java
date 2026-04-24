dbfe15dcb24849129bd6e1e6032729037359a944
hexaforce66 <hexaforce66@gmail.com>
2026-04-23 23:12:51 -0600

    Modernizacion Automatica HolaMundo

--- HolaMundo/HolaMundo.java
+++ HolaMundo/HolaMundo.java
@@ -1,11 +1,29 @@
-No hay código Java equivalente, ya que este programa COBOL es muy simple.
-Se podría implementar en Java como:
+class GestionSaldo {
 
-```java
-public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("--- INICIO DE TRANSACCION ---");
+        System.out.println("CLIENTE: Juan Perez");
+        double saldoInicial = 5000.00;
 
-  public static void main(String[] args) {
-    System.out.println("Hola Mundo");
-  }
+        char tipoTrans = 'R';
+        double monto = 1500.50;
+        
+        procesarTransaccion(tipoTrans, monto, saldoInicial);
+        System.out.println("--- FIN DE PROCESO ---");
+    }
+
+    private static void procesarTransaccion(char tipoTrans, double monto, double saldoActual) {
+        if (tipoTrans == 'D') {
+            saldoActual += monto;
+            System.out.println("DEPOSITO EXITOSO");
+        } else if (tipoTrans == 'R') {
+            if (saldoActual >= monto) {
+                saldoActual -= monto;
+                System.out.println("RETIRO EXITOSO");
+            } else {
+                System.out.println("SALDO INSUFICIENTE");
+            }
+        }
+        System.out.println("SALDO FINAL: " + saldoActual);
+    }
 }
-```
