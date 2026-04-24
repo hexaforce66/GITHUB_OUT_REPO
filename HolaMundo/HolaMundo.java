fd5ee9156dc894b7f4781be86ecdc78bcbf83bcc
hexaforce66 <hexaforce66@gmail.com>
2026-04-23 23:10:18 -0600

    Modernizacion Automatica HolaMundo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,11 @@
+No hay código Java equivalente, ya que este programa COBOL es muy simple.
+Se podría implementar en Java como:
+
+```java
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+  }
+}
+```
