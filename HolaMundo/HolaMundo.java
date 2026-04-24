33ac165f25553cbce80ab6c5d784a6dcb7ab4f10
hexaforce66 <hexaforce66@gmail.com>
2026-04-23 23:15:21 -0600

    Modernizacion Automatica HolaMundo

--- HolaMundo/HolaMundo.java
+++ HolaMundo/HolaMundo.java
@@ -2,28 +2,31 @@ class GestionSaldo {
 
     public static void main(String[] args) {
         System.out.println("--- INICIO DE TRANSACCION ---");
-        System.out.println("CLIENTE: Juan Perez");
-        double saldoInicial = 5000.00;
 
-        char tipoTrans = 'R';
-        double monto = 1500.50;
-        
-        procesarTransaccion(tipoTrans, monto, saldoInicial);
+        // Datos del cliente
+        int idCliente = 12345;
+        String nombreCliente = "JUAN PEREZ";
+        double saldoActual = 5000.00;
+
+        // Simulacion de transaccion
+        gestionarTransaccion('R', saldoActual);
+
         System.out.println("--- FIN DE PROCESO ---");
     }
 
-    private static void procesarTransaccion(char tipoTrans, double monto, double saldoActual) {
-        if (tipoTrans == 'D') {
-            saldoActual += monto;
+    private static void gestionarTransaccion(char tipoTrans, double saldo) {
+        if (tipoTrans.equals('D')) {
+            saldo += 1500.50;
             System.out.println("DEPOSITO EXITOSO");
-        } else if (tipoTrans == 'R') {
-            if (saldoActual >= monto) {
-                saldoActual -= monto;
+        } else if (tipoTrans.equals('R')) {
+            if (saldo >= 1500.50) {
+                saldo -= 1500.50;
                 System.out.println("RETIRO EXITOSO");
             } else {
                 System.out.println("SALDO INSUFICIENTE");
             }
         }
-        System.out.println("SALDO FINAL: " + saldoActual);
+
+        System.out.println("SALDO FINAL: " + saldo);
     }
 }
